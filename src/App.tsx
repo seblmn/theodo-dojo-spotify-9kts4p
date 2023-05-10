3d840d346fa272faeec20035f0fff64288ba9667
seblmn <133145441+seblmn@users.noreply.github.com>
2023-05-10 19:04:36 +0200

    1 file modified

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import logo from './assets/logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 const trackUrls = [
   'https://p.scdn.co/mp3-preview/742294f35af9390e799dd96c633788410a332e52',
@@ -8,6 +9,12 @@ const trackUrls = [
   'https://p.scdn.co/mp3-preview/0f6b8a3524ec410020457da4cdd7717f9addce2f',
   'https://p.scdn.co/mp3-preview/ac28d1b0be285ed3bfd8e9fa5fad133776d7cf36',
 ];
+
+let trackIndex = 0;
+
+const goToNextTrack = () => {
+  trackIndex += 1;
+}
 
 const App = () => {
   return (
@@ -20,7 +27,10 @@ const App = () => {
         <p>Pierre il est MDR</p>
       </div>
       <div className="App-buttons"></div>
-      <audio src={trackUrls[0]} autoPlay controls />
+      <audio src={trackUrls[trackIndex]} autoPlay controls />
+<button onClick={goToNextTrack}>
+    Next track
+</button>
     </div>
   );
 };
