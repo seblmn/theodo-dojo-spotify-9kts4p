ff53ef6208b366df979723172b665b256c4cbc9e
seblmn <133145441+seblmn@users.noreply.github.com>
2023-05-10 18:58:46 +0200

    1 file modified

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,14 @@
 import logo from './assets/logo.svg';
 import './App.css';
 
+const trackUrls = [
+  'https://p.scdn.co/mp3-preview/742294f35af9390e799dd96c633788410a332e52',
+  'https://p.scdn.co/mp3-preview/5a12483aa3b51331aba663131dbac967ccb33d99',
+  'https://p.scdn.co/mp3-preview/31f65b6a613010f22316c7be335b62226cf2f263',
+  'https://p.scdn.co/mp3-preview/0f6b8a3524ec410020457da4cdd7717f9addce2f',
+  'https://p.scdn.co/mp3-preview/ac28d1b0be285ed3bfd8e9fa5fad133776d7cf36',
+];
+
 const App = () => {
   return (
     <div className="App">
@@ -12,6 +20,7 @@ const App = () => {
         <p>Pierre il est MDR</p>
       </div>
       <div className="App-buttons"></div>
+      <audio src={trackUrls[0]} autoPlay controls />
     </div>
   );
 };
